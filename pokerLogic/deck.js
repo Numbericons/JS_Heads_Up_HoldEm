59ba7f245afe03c86285cf7605b3f44a2b7e1d1d
Zachary <zachjoliver@gmail.com>
2019-08-05 16:21:04 -0700

    Make deck class and tested in chrome console

--- pokerLogic/deck.js
+++ pokerLogic/deck.js
@@ -1,17 +1,15 @@
 class Deck {
   constructor(){
     this.cards_drawn = 0;
-    this.deck = newDeck();
+    this.cards = this.newDeck();
   }
+
   shuffle = (array) => {
     let counter = array.length;
 
     while (counter > 0) {
-      // Pick a random index
       let index = Math.floor(Math.random() * counter);
-      // Decrease counter by 1
       counter--;
-      // And swap the last element with it
       let temp = array[counter];
       array[counter] = array[index];
       array[index] = temp;
@@ -22,40 +20,14 @@ class Deck {
   newDeck = () => {
     let suits = ["s", "h", "d", "c"];
     let values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
-    deck = [];
-    for(let i = 0; i < suits.length; i++) {
-      for(let j = 0; j < values.length; j++) {
-        deck.push(values[j]+suits[i]);
+    let deck = [];
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < values.length; j++) {
+        deck.push(values[j] + suits[i]);
       }
     }
-    return shuffle(deck);
+    return this.shuffle(deck);
   }
 }
-
-// require_relative 'mycard.rb'
-// require 'colorize'
-
-// class Deck
-  
-
-//     attr_reader :deck, :cards_drawn
-
-//     def initialize(deck = Deck.random_deck)
-//         @deck = deck
-//         @cards_drawn = 0
-//     end
-
-//     def shuffle
-//         @deck = @deck.shuffle
-//     end
-
-//     def draw
-//         self.shuffle if cards_drawn % 52 == 0
-//         @cards_drawn+=1
-//         @deck.pop
-//     end
-
-//     def return(card)
-//         @deck.unshift(card)
-//     end
-// end
+// new Deck;
+console.log(new Deck)
