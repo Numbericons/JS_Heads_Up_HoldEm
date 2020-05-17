f3816cda4584f8c6dfc4e111841a5b860ecdf444
Zachary <zachjoliver@gmail.com>
2020-05-16 21:28:08 -0700

    Small tweak to index js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ $(() => {
   const actionsCont = $('.table-bottom-actions');
   const table = new Table(actionsCont, false, false); //3rd arg is watch mode
   table.setup();
-  
+});
+
+// $(() => {
   // let results = { player1: 0, player2: 0 }
   // const games = 1000
   // for (let z=0; z<games; z++) {
@@ -20,4 +22,4 @@ $(() => {
   // $(".result-text-1").text(`Comp Player 1 wins: ${results['player1']} games`)
   // $(".result-text-2").text(`Comp Player 2 wins: ${results['player2']} games`)
   // $(".stats-1").text(`Average hands per game: ${results['numHands'] / games}`);
-});
+// });
