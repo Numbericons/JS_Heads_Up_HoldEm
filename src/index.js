2e7ce78bb20ae601dba3c288bacabc6cf8fbd02a
Zachary <zachjoliver@gmail.com>
2020-05-16 21:27:39 -0700

    Adding stats to monte html page and to index file

--- src/index.js
+++ src/index.js
@@ -9,12 +9,15 @@ $(() => {
   const table = new Table(actionsCont, false, false); //3rd arg is watch mode
   table.setup();
   
-  // let wins = { player1: 0, player2: 0 }
-  // for (let z=0; z<1000; z++) {
+  // let results = { player1: 0, player2: 0 }
+  // const games = 1000
+  // for (let z=0; z<games; z++) {
   //   const table = new Table(null, true);
   //   const winner =  table.setup(true);
-  //   if (winner) wins[winner] += 1;
+    // if (winner) results[winner] += 1;
+    // results[numHands] += table.handNum;
   // }
-  // $(".result-text-1").text(`Comp Player 1 wins: ${wins['player1']} games`)
-  // $(".result-text-2").text(`Comp Player 2 wins: ${wins['player2']} games`)
+  // $(".result-text-1").text(`Comp Player 1 wins: ${results['player1']} games`)
+  // $(".result-text-2").text(`Comp Player 2 wins: ${results['player2']} games`)
+  // $(".stats-1").text(`Average hands per game: ${results['numHands'] / games}`);
 });
