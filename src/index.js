6fe78e8c0be20ef1b3f5ed029c23526cc8dea6e5
Zachary <zachjoliver@gmail.com>
2020-05-23 11:17:50 -0700

    Switch off watch mode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Table from './pokerLogic/table';
 
 $(() => {
   const actionsCont = $('.table-bottom-actions');
-  const table = new Table(actionsCont, false, true, true); //3rd arg is watch mode
+  const table = new Table(actionsCont, false, false, true); //3rd arg is watch mode
   table.setup();
 });
 
